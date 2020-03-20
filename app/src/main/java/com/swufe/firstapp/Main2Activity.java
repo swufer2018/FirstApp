dd670f4448a4b7cbc8c0ff7bd5342ebe820207dd
swufer2018 <1578278029@qq.com>
2020-03-20 19:56:17 +0800

    temperature

--- app/src/main/java/com/swufe/firstapp/Main2Activity.java
+++ app/src/main/java/com/swufe/firstapp/Main2Activity.java
@@ -3,12 +3,45 @@ package com.swufe.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    public Log log;
+    TextView temp;
+    TextView out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+        temp=(TextView)findViewById(R.id.inpText);
+        out=(TextView)findViewById(R.id.out);
+    }
+
+    @Override
+    public void onClick(View button) {
+        Transform();
+
+    }
+    public void Transform(){
+        log.i("main","transform");
+        String str=(String)temp.getText().toString();
+        if(str.length()==0){
+            out.setText("outPut:"+"error");
+        }
+        else{
+            double num=Double.parseDouble(str);
+            num=num*1.8+32;
+            out.setText("outPut:"+num);
+
+        }
+
     }
 }
