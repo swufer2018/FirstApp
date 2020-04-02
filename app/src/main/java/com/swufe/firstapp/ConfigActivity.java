2f7dd32f8c4e07225bb2bddd4f77682e12b75c8b
swufer2018 <1578278029@qq.com>
2020-04-02 16:02:07 +0800

    RatePlus

--- app/src/main/java/com/swufe/firstapp/ConfigActivity.java
+++ app/src/main/java/com/swufe/firstapp/ConfigActivity.java
@@ -2,13 +2,43 @@ package com.swufe.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ConfigActivity extends AppCompatActivity {
+    EditText dollarText;
+    EditText euroText;
+    EditText wonText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        Intent intent=getIntent();
+        float dollar2=intent.getFloatExtra("dollar_rate_key",0.0f);
+        float euro2=intent.getFloatExtra("euro_rate_key",0.0f);
+        float won2=intent.getFloatExtra("won_rate_key",0.0f);
+        dollarText=(EditText)findViewById(R.id.dollar_rate);
+        euroText=(EditText)findViewById(R.id.euro_rate);
+        wonText=(EditText)findViewById(R.id.won_rate);
+        dollarText.setText(String.valueOf(dollar2));
+        euroText.setText(String.valueOf(euro2));
+        wonText.setText(String.valueOf(won2));
+
+    }
+    public void save(View btn){
+        float newDollar=Float.parseFloat(dollarText.getText().toString());
+        float newEuro=Float.parseFloat(euroText.getText().toString());
+        float newWon=Float.parseFloat(wonText.getText().toString());
+        Intent intent=getIntent();
+        Bundle bd1=new Bundle();
+        bd1.putFloat("key_dollar",newDollar);
+        bd1.putFloat("key_euro",newEuro);
+        bd1.putFloat("key_won",newWon);
+        intent.putExtras(bd1);
+        setResult(2,intent);
+        finish();
     }
 }
--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -1,9 +1,13 @@
 package com.swufe.firstapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -35,18 +39,51 @@ public class RateActivity extends AppCompatActivity {
             Toast.makeText(this,"Input Error",Toast.LENGTH_SHORT).show();
         }
         if(btn.getId()==R.id.btn_dollar){
-            val=r*(1/7.1f);
+            val=r*dollarRate;
         }
         else if(btn.getId()==R.id.btn_euro){
-            val=r*(1/7.8f);
+            val=r*euroRate;
         }
         else if(btn.getId()==R.id.btn_won){
-            val=r*174f;
+            val=r*wonRate;
         }
         show.setText(val+"");
     }
     public void openOne(View btn){
-        Intent hello=new Intent(this,ConfigActivity.class);
-        startActivity(hello);
+        openConfig();
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this, ConfigActivity.class);
+        config.putExtra("dollar_rate_key", dollarRate);
+        config.putExtra("euro_rate_key", euroRate);
+        config.putExtra("won_rate_key", wonRate);
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.action_settings){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1 && resultCode==2){
+            Bundle bundle=data.getExtras();
+            dollarRate=bundle.getFloat("key_dollar",0.1f);
+            euroRate=bundle.getFloat("key_euro",0.1f);
+            wonRate=bundle.getFloat("key_won",0.1f);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
