67b2e063088ccc4fefcaa1e2b0339bef03e0ac05
swufer2018 <1578278029@qq.com>
2020-04-09 15:14:16 +0800

    SharedPerference,multithreadind,handler,html

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,13 +17,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class RateActivity extends AppCompatActivity implements Runnable{
     private final String TAG="Rate";
-    private float dollarRate=0.1f;
-    private float euroRate=0.2f;
-    private float wonRate=0.3f;
+    private float dollarRate=0.0f;
+    private float euroRate=0.0f;
+    private float wonRate=0.0f;
     EditText rmb;
     TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,21 @@ public class RateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rate);
         rmb=(EditText)findViewById(R.id.rmb);
         show=(TextView)findViewById(R.id.show);
+        SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        dollarRate=sharedPreferences.getFloat("dollar_rate",0.0f);
+        euroRate=sharedPreferences.getFloat("euro_rate",0.0f);
+        wonRate=sharedPreferences.getFloat("won_rate",0.0f);
+        Thread t=new Thread(this);
+        t.start();
+        handler=new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==5){
+
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
     public void onClick(View btn){
         String str=rmb.getText().toString();
@@ -37,6 +67,7 @@ public class RateActivity extends AppCompatActivity {
         }
         else{
             Toast.makeText(this,"Input Error",Toast.LENGTH_SHORT).show();
+            return;
         }
         if(btn.getId()==R.id.btn_dollar){
             val=r*dollarRate;
@@ -82,8 +113,44 @@ public class RateActivity extends AppCompatActivity {
             dollarRate=bundle.getFloat("key_dollar",0.1f);
             euroRate=bundle.getFloat("key_euro",0.1f);
             wonRate=bundle.getFloat("key_won",0.1f);
-
+            SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putFloat("dollar_rate",dollarRate);
+            editor.putFloat("euro_rate",euroRate);
+            editor.putFloat("won_rate",wonRate);
+            editor.commit();
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void run() {
+        Message msg=handler.obtainMessage(5);
+        URL url= null;
+        try {
+            url = new URL("https://www.usd-cny.com/icbc.htm");
+            HttpsURLConnection https= (HttpsURLConnection) url.openConnection();
+            InputStream in=https.getInputStream();
+            String html=inputStream2String(in);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize=1024;
+        final char[] buffer=new char[bufferSize];
+        final StringBuilder out=new StringBuilder();
+        Reader in=new InputStreamReader(inputStream,"gb2312");
+        for(;;){
+            int rsz=in.read(buffer,0,buffer.length);
+            if(rsz<0){
+                break;
+            }
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
